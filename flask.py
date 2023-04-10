6f80d4baadd43eb440394fc62eeb708edee04ff1
aadhav-argonaut <aadhav@argonaut.dev>
2023-04-10 14:53:22 +0530

    fix: use serverless-wsgi

--- flask.py
+++ flask.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import serverless_wsgi
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+def handler(event, context):
+    return serverless_wsgi.handle_request(app, event, context)
